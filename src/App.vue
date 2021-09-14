07a8e99458977ef12f8cef1a43e05012bb1b2ee8
timo <timo.guenzel@evia.de>
2021-09-14 08:59:21 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,96 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+        <HelloWorld msg="Vue"/>
+        <PersonForm @add:person="addPerson" />
+        <PersonList :persons=persons @delete:person="deletePerson" @edit:person="editPerson" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+  import PersonForm from './components/PersonForm.vue'
+  import PersonList from './components/PersonList.vue'
 
-export default {
+  const URL = process.env.VUE_APP_BACKEND_URL || 'set VUE_APP_BACKEND_URL in .env files!';
+  console.log('VUE_APP_BACKEND_URL: ', URL);
+
+  export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PersonForm,
+    PersonList,
+  },
+  data() {
+    return {
+        persons: []
+    }
+  },
+  mounted() {
+      this.getAllPersons()
+  },
+
+  methods: {
+    async getAllPersons() {
+        try {
+            const requestOptions = {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'}
+            };
+            const response = await fetch(URL, requestOptions)
+            this.persons = await response.json()
+            this.$forceUpdate();
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    async addPerson(name) {
+      try {
+          const requestOptions = {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({'name': name}),
+          };
+
+          await fetch(URL, requestOptions)
+
+          await this.getAllPersons()
+      } catch (error) {
+          console.error(error)
+      }
+    },
+
+    async deletePerson(person) {
+      try {
+          const requestOptions = {
+              method: 'DELETE',
+              headers: {'Content-Type': 'application/json'},
+          };
+
+          await fetch(URL + '/' + person.id, requestOptions);
+          this.persons = this.persons.filter(p => person.id !== p.id);
+      } catch (error) {
+          console.error(error);
+      }
+    },
+      async editPerson(person) {
+          try {
+              const requestOptions = {
+                  method: 'PUT',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify({name: person.name}),
+              };
+              await fetch(URL + '/' + person.id, requestOptions)
+          } catch (error) {
+              console.error(error)
+          }
+      }
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/PersonList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="personList" >
+        <p v-if="persons.length < 1" class="empty-table">
+            No data
+        </p>
+        <Person v-else :person="person" v-for="person in persons" :key="person.id"
+                @delete:person="deletePerson" @edit:person="editPerson"/>
+    </div>
+</template>
+
+<script lang="ts">
+    import Person from "@/components/Person";
+
+    export default {
+        name: "PersonList",
+        props: {
+            persons: {},
+        },
+        components: {
+            Person,
+        },
+        methods: {
+            deletePerson(person) {
+                this.$emit('delete:person', person)
+            },
+            editPerson(person) {
+                this.$emit('edit:person', person)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
